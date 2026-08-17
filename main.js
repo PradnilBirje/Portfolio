4e22e87c04394d290b40b59fe73ce51723c52c50
Pradnil Birje <pradnilbirje01@gmail.com>
2026-08-17 23:01:18 +0530

    Refactor main.js by removing unused code

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
 (function ($) {
     "use strict";
     
-    // loader
-    var loader = function () {
-        setTimeout(function () {
-            if ($('#loader').length > 0) {
-                $('#loader').removeClass('show');
-            }
-        }, 1);
-    };
-    loader();
-    
-    
     </> JavaScript
     // WOW animations disabled for smoother scrolling
     // new WOW().init();
@@ -41,22 +30,6 @@
         }
     });
     
-    
-    // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
-            
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
-        }
-    });
     
     // Skills
     $('.skills').waypoint(function () {
